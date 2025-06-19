cde2a89cd9ce52d80309ed8ca0c145c7636526fe
Ainz-fo <neoverse@outlook.fr>
2025-06-19 14:35:01 +0000

    Create set.js

---
+++ set.js
@@ -0,0 +1,7 @@
+module.exports = { 
+    SESSION_ID: process.env.SESSION_ID || 'ovl',
+    PREFIXE: process.env.PREFIXE || "+",            
+    MODE: process.env.MODE_PUBLIC || "oui",
+    DB: process.env.DB || 'postgres://neoverse:pomrleUMXwlmlpIcW2oFJmMX0CXzaFkf@dpg-combonun7f5s73d7uoog-a.oregon-postgres.render.com/neoverse_wz98',
+    WELCOME: process.env.WELCOME || 'oui',
+};
