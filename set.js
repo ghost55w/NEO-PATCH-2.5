d2d71d091cbd4bf724a958ad41aae9485de7c74a
Ainz-fo <neoverse@outlook.fr>
2025-06-19 14:37:14 +0000

    Update set.js

--- set.js
+++ set.js
@@ -2,6 +2,6 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || 'ovl',
     PREFIXE: process.env.PREFIXE || "+",            
     MODE: process.env.MODE_PUBLIC || "oui",
-    DB: process.env.DB || 'postgres://neoverse:pomrleUMXwlmlpIcW2oFJmMX0CXzaFkf@dpg-combonun7f5s73d7uoog-a.oregon-postgres.render.com/neoverse_wz98',
+    DB: process.env.DB || 'postgresql://postgres.zacfshdorkmodrenhalb:famillenignan1@@aws-0-eu-central-1.pooler.supabase.com:6543/postgres',
     WELCOME: process.env.WELCOME || 'oui',
 };
