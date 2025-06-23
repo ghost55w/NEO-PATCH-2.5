cbe6dcbcd5729ba16ae212ee22e75a9855aa0cec
Ainz-fo <neoverse@outlook.fr>
2025-06-23 22:09:39 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,7 +1,7 @@
 module.exports = { 
     SESSION_ID: process.env.SESSION_ID || 'ovl',
     PREFIXE: process.env.PREFIXE || "+",            
-    MODE: process.env.MODE_PUBLIC || "oui",
+    MODE: process.env.MODE_PUBLIC || "public",
     NUMERO_OWNER: process.env.NUMERO_OWNER || "22605463559",
     DB: process.env.DB || 'postgresql://postgres.zacfshdorkmodrenhalb:famillenignan1@@aws-0-eu-central-1.pooler.supabase.com:6543/postgres',
     WELCOME: process.env.WELCOME || 'oui',
