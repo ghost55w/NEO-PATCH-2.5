84300db2397c651ba271fd768f9d1120d945599a
Ainz-fo <neoverse@outlook.fr>
2025-07-02 10:20:10 +0000

    Update all_stars_menu.js

--- cmd/all_stars_menu.js
+++ cmd/all_stars_menu.js
@@ -1,5 +1,7 @@
 const { ovlcmd } = require("../lib/ovlcmd");
 
+const gifIntro = 'https://files.catbox.moe/7033mc.mp4';
+
 ovlcmd(
   {
     nom_cmd: "menuneo🔷",
@@ -9,6 +11,12 @@ ovlcmd(
   },
   async (ms_org, ovl, { arg, ms }) => {
     if (!arg || arg.length === 0) {
+      await ovl.sendMessage(ms_org, {
+        video: { url: gifIntro },
+        gifPlayback: true,
+        caption: ""
+      }, { quoted: ms });
+
       const lien = "https://files.catbox.moe/1ve4m6.jpg";
       const msg = "";
       await ovl.sendMessage(ms_org, { image: { url: lien }, caption: msg }, { quoted: ms });
@@ -25,6 +33,12 @@ ovlcmd(
   },
   async (ms_org, ovl, { arg, ms }) => {
     if (!arg || arg.length === 0) {
+      await ovl.sendMessage(ms_org, {
+        video: { url: gifIntro },
+        gifPlayback: true,
+        caption: ""
+      }, { quoted: ms });
+
       const lien = "https://files.catbox.moe/17sok6.jpg";
       const msg = "";
       await ovl.sendMessage(ms_org, { image: { url: lien }, caption: msg }, { quoted: ms });
@@ -41,6 +55,12 @@ ovlcmd(
   },
   async (ms_org, ovl, { arg, ms }) => {
     if (!arg || arg.length === 0) {
+      await ovl.sendMessage(ms_org, {
+        video: { url: gifIntro },
+        gifPlayback: true,
+        caption: ""
+      }, { quoted: ms });
+
       const liens = [
         "https://files.catbox.moe/mld35g.jpg",
         "https://files.catbox.moe/agapcu.jpg",
@@ -64,9 +84,45 @@ ovlcmd(
   },
   async (ms_org, ovl, { arg, ms }) => {
     if (!arg || arg.length === 0) {
+      await ovl.sendMessage(ms_org, {
+        video: { url: gifIntro },
+        gifPlayback: true,
+        caption: ""
+      }, { quoted: ms });
+
       const lien = "https://files.catbox.moe/l8p3xn.jpg";
       const msg = "";
       await ovl.sendMessage(ms_org, { image: { url: lien }, caption: msg }, { quoted: ms });
     }
+  }
+);
+
+ovlcmd(
+  {
+    nom_cmd: "pave",
+    classe: "Other",
+    react: "🎮",
+    desc: "Affiche le controller pave",
+  },
+  async (ms_org, ovl, { ms }) => {
+    await ovl.sendMessage(ms_org, {
+      video: { url: gifIntro },
+      gifPlayback: true,
+      caption: ""
+    }, { quoted: ms });
+
+    const texte = `░▒░ *🎮CONTROLLER📱*░▒░
+                          ▔▔▔▔▔▔▔                                     
+🎧 \`𝗖𝗵𝗮𝘁\`: 
+  ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔    
+🎮 *\`Actions\`*:
+
+🌀👊🏽 
+ 
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔                                     
+🌀 *ALL STARS⭐BATTLE ARENA🎮*   
+> NEOverse🔷 Text Gaming Esports Roleplay`;
+
+    await ovl.sendMessage(ms_org, { text: texte }, { quoted: ms });
   }
 );
