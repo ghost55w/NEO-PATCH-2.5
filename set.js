c7826d2c9a48accf56897f79facb0007941ecb41
ghost55w <103336705+ghost55w@users.noreply.github.com>
2025-10-27 16:22:35 +0100

    Delete set.js

--- set.js
+++
@@ -1,8 +0,0 @@
-module.exports = { 
-    SESSION_ID: process.env.SESSION_ID || 'ovl',
-    PREFIXE: process.env.PREFIXE || "+",            
-    MODE: process.env.MODE_PUBLIC || "public",
-    NUMERO_OWNER: process.env.NUMERO_OWNER || "22605463559",
-    DATABASE: process.env.DATABASE || 'postgresql://postgres.zacfshdorkmodrenhalb:famillenignan1@@aws-0-eu-central-1.pooler.supabase.com:6543/postgres',
-    WELCOME: process.env.WELCOME || 'oui',
-};
