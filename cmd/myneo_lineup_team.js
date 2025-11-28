5ac751a6c43e76c0d5c9d98ecbdfa0c28af2385e
Ainz-fo <neoverse@outlook.fr>
2025-11-28 21:23:13 +0000

    Update myneo_lineup_team.js

--- cmd/myneo_lineup_team.js
+++ cmd/myneo_lineup_team.js
@@ -21,9 +21,7 @@ ovlcmd({
     coupons: 0, gift_box: 0, all_stars: "", blue_lock: "+Team⚽", elysium: "+ElysiumMe💠"
   };
   const baseTeam = {
-    users: "aucun", team: "aucun", points_jeu: 0, rank: "aucun",
-    argent: 0, classement: "aucun", wins: 0, loss: 0,
-    draws: 0, championnats: 0, nel: 0, niveau: 0, trophies: 0, goals: 0
+    users: "aucun", team: "aucun", argent: 0, classement: "aucun", wins: 0, loss: 0, niveau: 0, trophies: 0, goals: 0
   };
   const baseLineup = {
     nom: "aucun", joueur1: "", joueur2: "", joueur3: "", joueur4: "",
