f41407cd76d44132cec8e67faa812a010135f1f1
Ainz-fo <neoverse@outlook.fr>
2025-11-29 13:42:30 +0000

    Update set.js

--- set.js
+++ set.js
@@ -5,4 +5,5 @@ module.exports = {
     NUMERO_OWNER: process.env.NUMERO_OWNER || "22605463559",
     DATABASE: process.env.DATABASE || 'postgresql://postgres.zacfshdorkmodrenhalb:famillenignan1@@aws-0-eu-central-1.pooler.supabase.com:6543/postgres',
     WELCOME: process.env.WELCOME || 'oui',
+    CARDS_NOMBRE: process.env.CARDS_NOMBRE || '10'
 };
